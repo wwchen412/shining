135038d2e75cf0cfd3557f575c21698671d14bd1
wwchen <wwchen412@gmail.com>
2017-09-25 15:02:27 +0800

    串連結

--- js/main.js
+++ js/main.js
@@ -9,19 +9,30 @@ $(function() {
     })
 })
 $(window).load(function() {
-    $('.menu').click(function() {
-        $('.menu-list').addClass('active').find('ul').addClass('active');
+        $('.menu').click(function() {
+            $('.menu-list').addClass('active').find('ul').addClass('active');
+
+        })
+        $('.menu-list').click(function() {
+            $('.menu-list').removeClass('active');
+        })
+        $('.menu-list ul li.close').click(function() {
+            $('.menu-list').removeClass('active');
+        })
+        $('.menu-list ul').click(function() {
+            //阻止泡泡往外找
+            event.stopPropagation();
+        })
 
     })
-    $('.menu-list').click(function() {
-        $('.menu-list').removeClass('active');
-    })
-    $('.menu-list ul li.close').click(function() {
-        $('.menu-list').removeClass('active');
-    })
-    $('.menu-list ul').click(function() {
-        //阻止泡泡往外找
-        event.stopPropagation();
-    })
+    // 切換圖片
 
+$(function() {
+    var big_img = $('.big-img img');
+    var first_img = $('.mini-imgs ul li:first-child img').attr('src');
+    big_img.attr('src', first_img);
+    $('.mini-imgs ul li img').click(function() {
+        var mini_img = $(this).attr('src');
+        big_img.attr('src', mini_img);
+    })
 })
