99f0c49b8fbfeb67f33c278fa1ea39f6d3779507
wwchen <wwchen412@gmail.com>
2017-09-25 17:28:34 +0800

    done

--- js/main.js
+++ js/main.js
@@ -7,28 +7,29 @@ $(function() {
         $(this).toggleClass('animation');
         $('#tea-nav li').toggleClass('slide');
     })
-})
+});
+// sandwich bar
 $(window).load(function() {
-        $('.menu').click(function() {
-            $('.menu-list').addClass('active').find('ul').addClass('active');
-            $('body').addClass('active');
-
-        })
-        $('.menu-list').click(function() {
-            $('.menu-list').removeClass('active');
-            $('body').removeClass('active');
-        })
-        $('.menu-list ul li.close').click(function() {
-            $('.menu-list').removeClass('active');
-            $('body').removeClass('active');
-        })
-        $('.menu-list ul').click(function() {
-            //阻止泡泡往外找
-            event.stopPropagation();
-        })
+    $('.menu').click(function() {
+        $('.menu-list').addClass('active').find('ul').addClass('active');
+        $('body').addClass('active');
 
     })
-    // 切換圖片
+    $('.menu-list').click(function() {
+        $('.menu-list').removeClass('active');
+        $('body').removeClass('active');
+    })
+    $('.menu-list ul li.close').click(function() {
+        $('.menu-list').removeClass('active');
+        $('body').removeClass('active');
+    })
+    $('.menu-list ul').click(function() {
+        //阻止泡泡往外找
+        event.stopPropagation();
+    })
+
+});
+// 切換圖片
 
 $(function() {
     var big_img = $('.big-img img');
@@ -38,4 +39,18 @@ $(function() {
         var mini_img = $(this).attr('src');
         big_img.attr('src', mini_img);
     })
-})
+});
+
+//  光箱
+$(function() {
+    $('.lightbox-group img').click(function() {
+        var img_src = $(this).attr('src');
+        $('body').append('<div class="overlay"><img src="' + img_src + '"></div>').addClass('active');
+        $('.overlay').click(function() {
+            $(this).remove();
+            $('body').removeClass('active');
+        })
+    })
+
+
+});
